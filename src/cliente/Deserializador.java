7482b539f029ca5604fcf29af85ff5ed82ef3267
sidneyfrancois <sidney.fvm@hotmail.com>
2017-09-09 17:29:05 -0300

    Adicionando a classe deserializar na parte do cliente

---
+++ src/cliente/Deserializador.java
@@ -0,0 +1,22 @@
+package cliente;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/*	Classe usada para deserializar os objetos que
+ * 	o cliente receber do servidor
+ * */
+
+public class Deserializador {
+	
+	public Object deserializar() throws ClassNotFoundException, IOException {
+		FileInputStream objetoRecebido = new FileInputStream("testa.ser");
+		ObjectInputStream entrada = new ObjectInputStream(objetoRecebido);
+		
+		Object objeto_deserializado = entrada.readObject();
+		entrada.close();
+		
+		return objeto_deserializado;
+	}
+}
